4953adb47676f1af20fc90745130e30e0d9cd484
Oskar Näsman <oskar.nasman@Oskars-MacBook-Pro.local>
2023-11-16 17:22:57 +0100

    update brewfile

--- script/automation.sh
+++ script/automation.sh
@@ -5,6 +5,8 @@ sudo spctl --master-disable ## Disables security stuff in catalina that will dri
 
 echo ">>>>>Installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" </dev/null ## /dev/null skips pressing enter for the installation
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/oskar.nasman/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo ">>>>>Bootstrapping all the applications for the first time, overwriting all"
 git clone https://github.com/onasman/jsfs-tool-dotfiles.git ~/.dotfiles
